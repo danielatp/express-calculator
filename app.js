53544a9efcf68cd794443cca0386b9b9c0521d82
Daniela Tizon <daniela@MacBook-Pro-de-Daniela.local>
2017-11-07 09:09:03 -0500

    app initialized

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+# Express Calculator
+
+Create a simple calculator app using Express.
+
+You will create an express calculator application with four separate GET routes: `add`, `sub`, `mult` and `div`. All routes take in two number as parameters. When the route is hit, your browser should display the result of the math operation and the numbers two numbers on the screen.
+
+For Example:
+
+- When a user visits /add/9/3, it should display 12
+- When a user visits /sub/9/3, it should display 6
+- When a user visits /mult/9/3, it should display 27
+- When a user visits /div/9/3, it should display 3
+
+
+Bonus: Handle decimals
+Other Bonus: Style the page. Serve a stylesheet in yout express app!
+*/
+
